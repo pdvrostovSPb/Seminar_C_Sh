dd9664f8a7b0f796eb713fbf731259bbe228c651
pdvrostovSPb <pdvrostov@mail.ru>
2022-06-10 09:26:56 +0300

    completed the first homework

--- Seminar01/Program.cs
+++ Seminar01/Program.cs
@@ -1,16 +1,28 @@
-﻿/* Console.WriteLine("Введите число а ");
+﻿/* Задача 2: Напишите программу, которая на вход
+принимает два числа и выдаёт, какое число большее, а
+какое меньшее. */
+
+/* Console.WriteLine("Введите число а ");
 Console.WriteLine("Введите число b ");
 
 int a = int.Parse(Console.ReadLine());
 int b = int.Parse(Console.ReadLine());
 
 if (a > b)
-  Console.WriteLine("max = a");
+{
+    Console.WriteLine("max = " + a);
+    Console.WriteLine("min = " + b);
+}
 else
-  Console.WriteLine("max = b");
- */
+{
+    Console.WriteLine("max = " + b);
+    Console.WriteLine("min = " + a);
+}  */
 
-//////////
+//////////////////////////////////////
+
+/* Задача 4: Напишите программу, которая принимает на
+вход три числа и выдаёт максимальное из этих чисел. */
 
 /* Console.WriteLine("Введите число а ");
 Console.WriteLine("Введите число b ");
@@ -21,17 +33,20 @@ int b = int.Parse(Console.ReadLine());
 int c = int.Parse(Console.ReadLine());
 
 if (a > b)
-  if(a > c)
-    Console.WriteLine("max = " + a);
-  else
-    Console.WriteLine("max = " + c);
+    if (a > c)
+        Console.WriteLine(a);
+    else
+        Console.WriteLine(c);
 else
-  if(b > c)
-    Console.WriteLine("max = " + b);
-  else
-    Console.WriteLine("max = " + c); */
+    if (b > c)
+        Console.WriteLine(b);
+    else
+        Console.WriteLine(c); */
 
-    ////////
+/////////////////////////////////////
+
+/* Задача 6: Напишите программу, которая на вход
+принимает число и выдаёт, является ли число чётным  */
 
 /* Console.WriteLine("Введите число");
 
@@ -42,18 +57,25 @@ if(n % 2 == 0)
 else
   Console.WriteLine("Число нечетное"); */
 
-///////////
+/////////////////////////////////////
 
-Console.WriteLine("Введите число");
+/* Задача 8: Напишите программу, которая на вход
+принимает число (N), а на выходе показывает все чётные
+числа от 1 до N. */
+
+/* Console.WriteLine("Введите число");
 
 int n = int.Parse(Console.ReadLine());
 
-int i = 0;
+if(n == 1)
+  Console.WriteLine("Четных чисел нет");
+
+int i = 2;
 
 while(i <= n)
 {
     Console.WriteLine(i);
     i += 2;
 }
-
-  
+ */
+ 
