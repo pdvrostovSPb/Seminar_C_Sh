016d1601ba4f7588174d900a01b9d81ba11bd1e2
pdvrostovSPb <pdvrostov@mail.ru>
2022-06-28 16:14:10 +0300

    added Seminar07

---
+++ Seminar07/Program.cs
@@ -0,0 +1,158 @@
+﻿/* Задайте двумерный массив размером m×n,
+заполненный случайными целыми числами.
+m = 3, n = 4.
+1 4 8 19
+5 -2 33 -2
+77 3 8 1 */
+
+/* void Print(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+
+}
+int m = 3, n = 4;
+int[,] mass = new int[m, n];
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        mass[i, j] = new Random().Next(0, 10);
+    }
+}
+
+Print(mass);
+Console.WriteLine();
+Print(mass);
+ */
+
+/* Задача 48.
+Задайте двумерный массив, каждый элемент которого равен сумме собственных индексов. */
+
+/* void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + "  ");
+
+        }
+        Console.WriteLine();
+    }
+}
+int m = 7, n = 5;
+int[,] mass = new int[m, n];
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        mass[i, j] = i + j;
+    }
+}
+PrintArray(mass); */
+
+/* Задача 49: Задайте двумерный массив. Найдите элементы, у
+которых оба индекса чётные, и замените эти элементы на их
+квадраты. */
+
+/* Задача 51: Задайте двумерный массив. Найдите сумму
+элементов, находящихся на главной диагонали (с индексами
+(0,0); (1;1) и т.д. */
+
+//////////////////////////////////////////////////////////////
+
+/* Задача 47: Задайте двумерный массив размером m×n,
+заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9 */
+
+/* void PrintArray(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + "     ");
+        }
+        Console.WriteLine();
+    }
+}
+int m = 5, n = 4;
+double[,] mass = new double[m, n];
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        mass[i, j] = new Random().Next(-9, 9) + new Random().NextDouble();
+        mass[i, j] = Math.Round(mass[i, j], 2);
+    }
+}
+PrintArray(mass); */
+
+/////////////////////////////////////////////////////////////
+
+/* Задача 50: Напишите программу, которая на вход
+принимает позиции элемента в двумерном массиве, и
+возвращает значение этого элемента или же указание,
+что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет */
+
+
+/* Задача 52: Задайте двумерный массив из целых чисел.
+Найдите среднее арифметическое элементов в каждом
+столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого
+столбца: 4,6; 5,6; 3,6; 3. */
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + "  ");
+
+        }
+        Console.WriteLine();
+    }
+}
+int m = 4, n = 3;
+int[,] mass = new int[m, n];
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        mass[i, j] = new Random().Next(0, 10);
+    }
+}
+PrintArray(mass);
+
+Console.Write("Среднее арифметическое каждого столбца: ");
+
+float sum = 0;
+for (int i = 0; i < mass.GetLength(1); i++)
+{
+    for (int j = 0; j < mass.GetLength(0); j++)
+    {
+        sum += mass[j, i];
+    }
+    Console.Write(sum / m + "," + sum % m + " , ");
+    sum = 0;
+}
