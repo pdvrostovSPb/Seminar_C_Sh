2c4880d4e76a8ab7237f21245b23695015bbcb8e
pdvrostovSPb <pdvrostov@mail.ru>
2022-06-29 13:42:20 +0300

    added Seminar07HW whith tasks 47, 50, 52

--- Seminar07/Program.cs
+++ Seminar07/Program.cs
@@ -68,91 +68,19 @@ PrintArray(mass); */
 
 //////////////////////////////////////////////////////////////
 
-/* Задача 47: Задайте двумерный массив размером m×n,
-заполненный случайными вещественными числами.
-m = 3, n = 4.
-0,5 7 -2 -0,2
-1 -3,3 8 -9,9
-8 7,8 -7,1 9 */
 
-/* void PrintArray(double[,] arr)
+
+// В случайных точках консоли вывести случайные числа
+
+
+/* Random r = new Random();
+Console.CursorVisible = false;
+while(true)
 {
-    for (int i = 0; i < arr.GetLength(0); i++)
-    {
-        for (int j = 0; j < arr.GetLength(1); j++)
-        {
-            Console.Write(arr[i, j] + "     ");
-        }
-        Console.WriteLine();
-    }
-}
-int m = 5, n = 4;
-double[,] mass = new double[m, n];
-for (int i = 0; i < mass.GetLength(0); i++)
-{
-    for (int j = 0; j < mass.GetLength(1); j++)
-    {
-        mass[i, j] = new Random().Next(-9, 9) + new Random().NextDouble();
-        mass[i, j] = Math.Round(mass[i, j], 2);
-    }
-}
-PrintArray(mass); */
-
-/////////////////////////////////////////////////////////////
-
-/* Задача 50: Напишите программу, которая на вход
-принимает позиции элемента в двумерном массиве, и
-возвращает значение этого элемента или же указание,
-что такого элемента нет.
-Например, задан массив:
-1 4 7 2
-5 9 2 3
-8 4 2 4
-17 -> такого числа в массиве нет */
-
-
-/* Задача 52: Задайте двумерный массив из целых чисел.
-Найдите среднее арифметическое элементов в каждом
-столбце.
-Например, задан массив:
-1 4 7 2
-5 9 2 3
-8 4 2 4
-Среднее арифметическое каждого
-столбца: 4,6; 5,6; 3,6; 3. */
-
-void PrintArray(int[,] arr)
-{
-    for (int i = 0; i < arr.GetLength(0); i++)
-    {
-        for (int j = 0; j < arr.GetLength(1); j++)
-        {
-            Console.Write(arr[i, j] + "  ");
-
-        }
-        Console.WriteLine();
-    }
-}
-int m = 4, n = 3;
-int[,] mass = new int[m, n];
-for (int i = 0; i < mass.GetLength(0); i++)
-{
-    for (int j = 0; j < mass.GetLength(1); j++)
-    {
-        mass[i, j] = new Random().Next(0, 10);
-    }
-}
-PrintArray(mass);
-
-Console.Write("Среднее арифметическое каждого столбца: ");
-
-float sum = 0;
-for (int i = 0; i < mass.GetLength(1); i++)
-{
-    for (int j = 0; j < mass.GetLength(0); j++)
-    {
-        sum += mass[j, i];
-    }
-    Console.Write(sum / m + "," + sum % m + " , ");
-    sum = 0;
-}
+    Console.SetCursorPosition(
+        left: r.Next(Console.WindowWidth),
+        top: r.Next(Console.WindowHeight)
+    );
+    Console.Write(r.Next(10));
+    Thread.Sleep(1000);
+} */
