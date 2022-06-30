6c14e4b3d246590dc1db76de1c4f450b33287346
pdvrostovSPb <pdvrostov@mail.ru>
2022-06-30 16:21:34 +0300

    modified Seminar06, added the few new tasks

--- Seminar06/Program.cs
+++ Seminar06/Program.cs
@@ -70,7 +70,7 @@ x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются
 пользователем.
 b1 = 2, k1 = 5, b2 = 4, k2 =
 9 -> (-0,5; 5,5) */
-Console.WriteLine("Введите число b1 ");
+/* Console.WriteLine("Введите число b1 ");
 double b1 = double.Parse(Console.ReadLine());
 Console.WriteLine("Введите число k1 ");
 double k1 = double.Parse(Console.ReadLine());
@@ -81,7 +81,7 @@ double k2 = double.Parse(Console.ReadLine());
 double x = (b2-b1)/(k1-k2);
 double y = k1*x + b1;
 
-Console.WriteLine(x + "," + y);
+Console.WriteLine(x + "," + y); */
 
 /* if(y == k2*x + b2)
  {
