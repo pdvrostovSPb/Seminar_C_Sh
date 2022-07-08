761fa1ddb5512a45ae5ebc6ef209075c94e563cb
pdvrostovSPb <pdvrostov@mail.ru>
2022-07-08 12:16:03 +0300

    added solutions to tasks 64, 66, 68

--- Seminar09HW/Program.cs
+++ Seminar09HW/Program.cs
@@ -34,8 +34,28 @@ Rec(M, N); */
 M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30 */
 
+/* int Sum(int a, int b)
+{
+    if (a == b) return a;
+    {
+        return Sum(a + 1, b) + a;
+    }
+}
+int M = 4; int N = 8;
+Console.WriteLine(Sum(M, N)); */
+
 ///////////////////////////////////////////////////////////
 
 /* Задача 68: Напишите программу вычисления функции Аккермана с помощью
 рекурсии. Даны два неотрицательных числа m и n.
 m = 3, n = 2 -> A(m,n) = 29 */
+
+/* int Akkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (m > 0 && n == 0) return Akkerman(m - 1, 1);
+    if (m > 0 && n > 0) return Akkerman(m - 1, Akkerman(m, n - 1));
+    return 0;
+}
+
+Console.WriteLine(Akkerman(3,2)); */
